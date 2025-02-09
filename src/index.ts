4296196dfc80ce3269ba7a0c59581a5f871de50b
Emma Gray <emma.possibly@gmail.com>
2025-02-09 15:57:04 +0000

    use validators in fetchData function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-export async function fetchData() {
+import {Job, Member, validateJobs, validateMembers} from './validators';
+
+async function fetchData(): Promise<{members: Member[]; jobs: Job[]}> {
   const members = await fetch(
     'https://bn-hiring-challenge.fly.dev/members.json',
   )
     .then(response => response.json())
     .catch(error => console.error(error));
+  validateMembers(members);
+
   const jobs = await fetch('https://bn-hiring-challenge.fly.dev/jobs.json')
     .then(response => response.json())
     .catch(error => console.error(error));
+  validateJobs(jobs);
 
-  console.log('MEMBERS:');
-  console.log(members);
-
-  console.log('\n\n\n=====\n\n\n');
-  console.log('JOBS:');
-  console.log(jobs);
+  return {members, jobs};
 }
 
-void fetchData();
+const {members, jobs} = await fetchData();
+
+console.log('MEMBERS:');
+console.log(members);
+
+console.log('\n=====\n');
+console.log('JOBS:');
+console.log(jobs);
---
+++ src/validators/index.ts
@@ -0,0 +1,5 @@
+export {validateJobs} from './validateJobs';
+export {validateMembers} from './validateMembers';
+
+export type {Job} from './validateJobs';
+export type {Member} from './validateMembers';
