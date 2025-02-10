19dcdd96687612d2bc92de91123333a9698a79f9
Emma Gray <emma.possibly@gmail.com>
2025-02-10 12:04:48 +0000

    refactor filterByLocation

--- src/filters/filterByLocation.ts
+++ src/filters/filterByLocation.ts
@@ -15,45 +15,32 @@ const tokenizer = new WordTokenizer();
  * to the provided member, based on their bio.
  */
 export function filterByLocation({member, jobs}: FilterByLocationOptions) {
-  const uniqueLocations = [
-    ...new Set(jobs.map(job => job.location.toLowerCase())),
-  ];
-
   const {bio} = member;
   const tokens = tokenizer.tokenize(bio.toLowerCase());
   const stems = tokens.map(token => PorterStemmer.stem(token));
 
-  const foundLocations = stems.filter(stem => uniqueLocations.includes(stem));
+  const relevantJobs = jobs.filter(job => {
+    const {location} = job;
+    const locationStem = PorterStemmer.stem(location.toLowerCase());
 
-  // Return unfiltered list if no relevant locations in bio
-  if (foundLocations.length === 0) return jobs;
-
-  // Bio specifies a location to exclude
-  const outsideStem = PorterStemmer.stem('outside');
-  if (stems.includes(outsideStem)) {
-    const index = stems.indexOf(outsideStem);
-    const locationToExclude = stems
-      .slice(index)
-      .find(stem => uniqueLocations.includes(stem));
-    if (locationToExclude)
-      return jobs.filter(
-        job => job.location.toLowerCase() !== locationToExclude,
+    // Bio specifies a location to exclude
+    const outsideStem = PorterStemmer.stem('outside');
+    if (stems.includes(outsideStem))
+      return (
+        !stems.includes(locationStem) ||
+        stems.indexOf(locationStem) < stems.indexOf(outsideStem)
       );
-  }
 
-  // Bio contains a target location to prioritise over other locations mentioned
-  const relocateStem = PorterStemmer.stem('relocate');
-  if (stems.includes(relocateStem)) {
-    const index = stems.indexOf(relocateStem);
-    const targetLocation = stems
-      .slice(index)
-      .find(stem => uniqueLocations.includes(stem));
-    if (targetLocation)
-      return jobs.filter(job => job.location.toLowerCase() === targetLocation);
-  }
+    // Bio indicates intention to relocate
+    const relocateStem = PorterStemmer.stem('relocate');
+    if (stems.includes(relocateStem))
+      return (
+        stems.includes(locationStem) &&
+        stems.indexOf(relocateStem) < stems.indexOf(locationStem)
+      );
 
-  // Bio contains unmodified location(s) to include
-  return jobs.filter(job =>
-    foundLocations.includes(job.location.toLowerCase()),
-  );
+    return stems.includes(locationStem);
+  });
+
+  return relevantJobs.length > 0 ? relevantJobs : jobs;
 }
